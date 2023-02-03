22f06987e77bde634474addbda609c7033561667
eriksik2 <grettireirikur@gmail.com>
2023-02-03 22:22:26 +0100

    Changed default configuration

--- src/utils/firstOpenAppModel.ts
+++ src/utils/firstOpenAppModel.ts
@@ -2,6 +2,7 @@ import { DateTimeWidget } from 'components/Widgets/DateTimeWidget';
 import { LinksWidget } from 'components/Widgets/LinksWidget';
 import { QuoteWidget } from 'components/Widgets/QuoteWidget';
 import { WeatherWidget } from 'components/Widgets/WeatherWidget';
+import { YoutubeWidget } from 'components/Widgets/YoutubeWidget';
 import { AppModel } from 'DataModel/AppModel';
 import { GridBoardModel } from 'DataModel/GridBoardModel';
 import { WidgetDescriptor } from 'widgets/WidgetDescriptor';
@@ -52,8 +53,28 @@ export function firstOpenAppModel() {
     boardModel2.addWidget(new WidgetDescriptor(QuoteWidget, {
     }), { row: 1, col: 2, width: 1, height: 2 });
 
+    const boardModel3 = new GridBoardModel(3, 4);
+    boardModel3.addWidget(new WidgetDescriptor(YoutubeWidget, {
+      url: "https://www.youtube.com/watch?v=BfKanl1aSG0",
+    }), { row: 1, col: 1, width: 3, height: 2 });
+    boardModel3.addWidget(new WidgetDescriptor(QuoteWidget, {
+    }), { row: 0, col: 0, width: 3, height: 1 });
+    boardModel3.addWidget(new WidgetDescriptor(LinksWidget, {
+      links: [
+        { name: "Google", url: "https://google.com" },
+        { name: "Reddit", url: "https://reddit.com" },
+      ],
+    }), { row: 0, col: 3, width: 1, height: 1 });
+    boardModel3.addWidget(new WidgetDescriptor(LinksWidget, {
+      links: [
+        { name: "Google", url: "https://google.com" },
+        { name: "Reddit", url: "https://reddit.com" },
+      ],
+    }), { row: 2, col: 0, width: 1, height: 1 });
+
     const appModel = new AppModel();
     appModel.addBoard(boardModel1);
     appModel.addBoard(boardModel2);
+    appModel.addBoard(boardModel3);
     return appModel;
 }
